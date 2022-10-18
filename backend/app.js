18c18f636320b9d442e0340c2c30cf0a89008638
Alexandre <107415236+AlexLeoty@users.noreply.github.com>
2022-10-18 17:59:54 +0200

    Commit piiquante

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const sauceRoutes = require('./routes/sauces')
+const userRoutes = require('./routes/user');
+const path = require('path');
+
+
+mongoose.connect('mongodb+srv://'+ process.env.DB_USER_PASS +'@cluster0.dmvpehz.mongodb.net/?retryWrites=true&w=majority',
+{UseNewUrlParser: true,
+useUnifiedTopology: true})
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'origin, X-Requested-With, Content, Accept, Content-type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+
+
+app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname,'images')));
+
+module.exports = app;
